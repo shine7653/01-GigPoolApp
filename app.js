259bae547f8d3feacd57a37b1543a031fedc7272
Sinae Kim <54830877+shine7653@users.noreply.github.com>
2019-12-31 22:12:11 -0800

    heroku confused

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
+
   
 // Database ---> after this, create a model Gig.js
 const db = require('./config/database');
@@ -29,6 +30,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // Index route
 // app.get('/', (req, res) => res.send('INDEX'));
--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
 // Option 1: Passing parameters separately
-module.exports = new Sequelize('codegigsDB', 'root', '~Ma009090', {
-    host: 'localhost',
+module.exports = new Sequelize('swyn1f8m8n0cfy8z', 'lxlkww5lzbgnzvj4', 'kr2eie3njdw1u1e0', {
+    host: 'qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
 
     pool: {
